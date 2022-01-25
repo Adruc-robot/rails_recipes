bddb4b1cd5e222be4f8f415e33d2921e9f307518
Daniel Curda <dgcurda@gmail.com>
2022-01-24 20:44:01 -0600

    updating a migration since pg is less forgiving with boolen

--- db/migrate/20220119220655_add_this_to_that.rb
+++ db/migrate/20220119220655_add_this_to_that.rb
@@ -1,6 +1,6 @@
 class AddThisToThat < ActiveRecord::Migration[7.0]
   def change
-    change_column :ingredients, :global, :boolen, :default => false
+    change_column :ingredients, :global, :boolean, :default => false
     change_column :states, :global, :boolean, :default => false
     change_column :units, :global, :boolean, :default => false
   end
