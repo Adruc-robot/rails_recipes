1f78153b3a4acf3336859ba1094210305b4c25e9
Daniel Curda <dgcurda@gmail.com>
2022-01-24 20:00:16 -0600

    fix for first time users visiting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      if current_user.admin
+        @recipes = Recipe.all.order(name: :asc)
+      else
+        @recipes = current_user.recipes.or(Recipe.where(global: "T")).order(name: :asc)
+      end
+    else
+        @recipes = Recipe.where(global: "T").order(name: :asc)
+    end
   end
   def about
   end
