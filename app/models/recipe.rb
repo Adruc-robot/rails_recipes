0e45308c5f58a757c1366e212d5c2acadbda5145
Daniel Curda <dgcurda@gmail.com>
2022-02-05 12:01:55 -0600

    completed initial push to make a builder, styling to come

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
     belongs_to :user
-    has_many :recipe_steps
-    has_many :recipe_ingredients
+    has_many :recipe_steps, dependent: :destroy
+    has_many :recipe_ingredients, dependent: :destroy
 end
--- db/migrate/20220122182556_dothingtothing.rb
+++ db/migrate/20220122182556_dothingtothing.rb
@@ -2,6 +2,6 @@ class Dothingtothing < ActiveRecord::Migration[7.0]
   def change
     change_column :recipes, :global, :string, :default => "IP"
     remove_column :recipe_ingredients, :global
-    remove_column :recipe_states, :global
+    #remove_column :recipe_states, :global
   end
 end
