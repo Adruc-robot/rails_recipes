32b718fed790cb50c2a14cb41fa0a66fbb79ca9d
Daniel Curda <dgcurda@gmail.com>
2022-02-06 09:13:18 -0600

    fix for only seeing personal or global, published recipes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      if current_user.admin
+      if current_user.admin == "T"
         @recipes = Recipe.all.order(name: :asc)
       else
-        @recipes = current_user.recipes.or(Recipe.where(global: "T")).order(name: :asc)
+        @recipes = current_user.recipes.or(Recipe.where(global: "T", status: "PB")).order(name: :asc)
       end
     else
         @recipes = Recipe.where(global: "T", status: "PB").order(name: :asc)
